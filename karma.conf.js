997adba9f519468f4a86033cc1eea65c4cc5416f
Carlos Roberto de Oliveira Maciel <carlos.maciel@trescon.com.br>
2022-11-10 00:57:48 -0300

    ajustando deploy

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/restauranttemplate2'),
+      dir: require('path').join(__dirname, './coverage/hamgus-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'restauranttemplate2';
+  title = 'hamgus-frontend';
 }
